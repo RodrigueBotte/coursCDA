5db9eccb26e86913f1fca2c556c3a27a675ceac8
Rodrigue Botte <rodrigue.botte@gmail.com>
2025-05-06 10:56:24 +0200

    début du cours de DOM

---
+++ DOM/script.js
@@ -0,0 +1,46 @@
+let hello = ("Hello World");
+console.log(hello);
+
+let maVoiture = {
+    marque: "Peugeot",
+    modele: "308",
+    annee: 2020,
+    couleur: "rouge"
+}
+console.log(maVoiture);
+
+let maVoiture2 = new Object();
+maVoiture2.marque = "Citroen";
+maVoiture2.modele = "C4";
+maVoiture2.annee = 2021;
+maVoiture2.couleur = "bleu";
+console.log(maVoiture2);
+
+let deux = 2;
+let trois = 3;
+let quatre = 4;
+
+if (deux == 2) {
+    console.log("deux est égal à 2");
+}
+else {
+    console.log("deux n'est pas égal à 2");
+}
+
+if (trois == 3) {
+    console.log("trois est égal à 3");  
+    
+}
+else {
+    console.log("trois n'est pas égal à 3");
+}
+
+if (quatre == 4) {
+    console.log("quatre est égal à 4");
+    
+}
+else {
+    console.log("quatre n'est pas égal à 4");
+
+    
+}
