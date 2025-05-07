2347567a890b258dd43bd966fe03029cd67e32b2
Rodrigue Botte <rodrigue.botte@gmail.com>
2025-05-07 16:19:50 +0200

    modification du script pour la connexion et l'inscription

--- exoFilmPref/script.js
+++ exoFilmPref/script.js
@@ -1,6 +1,6 @@
 // mise en place du switch de langue
 const lang = document.querySelector(".lang");
-lang.addEventListener("click", () => {
+lang?.addEventListener("click", () => {
   if (lang.textContent === "Fr") {
     lang.style.left = "82%";
     lang.textContent = "Elf";
@@ -15,5 +15,46 @@ lang.addEventListener("click", () => {
     lang.style.transition = "0.5s";
     document.body.style.fontFamily = "classique";
   }
+});
+
+// mise en place de l'enregistrement de compte
+const pseudo = document.getElementById("pseudo");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordBis = document.getElementById("passwordBis");
+const registerForm = document.getElementById("registerForm");
+
+function register() {
+  if (password.value != passwordBis.value) {
+    alert("Les mots de passe ne correspondent pas");
+  } else {
+    localStorage.setItem("pseudo", pseudo.value);
+    localStorage.setItem("email", email.value);
+    localStorage.setItem("password", password.value);
+    localStorage.setItem("passwordBis", passwordBis.value);
+    window.location.href = "/coursCDA/exoFilmPref/connexion.html";
+  }
+}
+
+registerForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  register();
+});
+
+const pseudoCo = document.getElementById("pseudoConnexion");
+const passwordCo = document.getElementById("passwordConnexion");
+function connexion() {
+  let pseudoVerif = localStorage.getItem("pseudo");
+  let passwordVerif = localStorage.getItem("password");
+  if (pseudoVerif != pseudoCo.value || passwordVerif != passwordCo.value) {
+    alert("Le mot de passe ou le pseudo est incorrect");
+  } else {
+    window.location.href = "/coursCDA/exoFilmPref/film.html";
+  }
+}
+
+document.getElementById("loginForm")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  connexion();
 });
 
